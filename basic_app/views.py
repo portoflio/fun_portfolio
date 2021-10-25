c17c59b5e999d539e32ed6bd94fd2d1ed7565e65
Harrison Katantazi <harrison.katantazi@gmail.com>
2021-10-25 22:50:48 +0200

    update_contact_form

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.core.mail import message, send_mail, BadHeaderError
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+#if the user requests to send a form, this checks if the form is valid
+        if form.is_valid():
+            subject = "Send Us a message"
+            bodyc= {
+            'first_name': forms.cleaned_data['first_name'],
+            'last_name':  form.cleaned_data['last_name'],
+            'email': form.cleaned_data['email_address'],
+            'message': form.cleaned_data['message'],
+
+            }
+            #joining all the body's values together to format the actual email
+            message = "\n".join(bodyc.values)
+
+            try:
+                send_mail(subject, message, 'admin@example.com',['admin@example.com'])
+            except BadHeaderError:
+                return HttpResponse('invalid header found.')
+            return redirect("main:homepage")
+        form = ContactForm()
+        return render(request, "main/contact.html",{'form':form})
+
+    
+    
