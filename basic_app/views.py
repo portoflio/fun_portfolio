1516b77086e360a55f79765793c48a7135cd1b34
DuyguCM <duygu.celik.morkoc@gmail.com>
2021-10-26 10:23:36 +0200

    designing home page, beginning profile page, css configurations by Duygu

--- basic_app/views.py
+++ basic_app/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def profile(request):
+    str_req = str(request)    
+    if 'amr' in str_req:
+        title = 'Amr'
+    elif 'duygu' in str_req:
+        title = 'Duygu'
+    elif 'harrison' in str_req:
+        title = 'Harrison'
+    elif 'pierre' in str_req:
+        title = 'Pierre'
+    elif 'robert' in str_req:
+        title = 'Robert'
+    else:
+        title = str_req
+    return render(request, 'profile.html', {'title':title})
