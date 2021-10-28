7ab07b0a6d3895ffa803ad8c4f2452f76bbd6dd7
Pierre Iversen <pierreiverssen@hotmail.com>
2021-10-28 17:50:56 +0200

    solved model problem

--- basic_app/admin.py
+++ basic_app/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from basic_app.models import SubscribeModel
 from basic_app.models import Subscribers
 
 # Register your models here.
-admin.site.register(SubscribeModel)
 admin.site.register(Subscribers)
--- basic_app/models.py
+++ basic_app/models.py
@@ -3,12 +3,6 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
-class SubscribeModel(models.Model):
-    email = models.EmailField(null=False, max_length=200, unique=True)
-
-    def __str__(self):
-        return self.email
-
 class Subscribers(models.Model):
 
     email = models.EmailField(unique=True, null=False)
